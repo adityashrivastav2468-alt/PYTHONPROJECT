823ffa96b73528e07af6cb9892315e4f1c9823da
Aditya Shrivastav <aditya.shrivastav2468@gmail.com>
2026-01-12 09:24:05 +0545

    added new code

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
-from flask import Flask
+from flask import Flask,render_template
 
 app= Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_templates('index.html',name= 'I am Aditya')
+    return render_template('index.html',name= 'I am Aditya')
 @app.route("/about")
 def about():
     return 'I am Aditya.'
+@app.route("/contact")
+def contact():
+    return render_template('contact.html')
